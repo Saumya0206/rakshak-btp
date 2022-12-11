011650eba364c2675a40adbaf7641430fe99879a
Soumya Vaish <vaish.1@iitj.ac.in>
2022-12-11 19:27:53 +0530

    measurement page completed

--- lib/main_screens/Measurement.dart
+++ lib/main_screens/Measurement.dart
@@ -7,6 +7,9 @@ import 'package:google_sign_in/google_sign_in.dart';
 import '../results_screen/Done.dart';
 import 'package:rakshak/main_screens/TestPage.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'dart:math';
+import 'package:rakshak/main_screens/PopupResult.dart';
+import 'package:flutter/services.dart';
 
 // bool _wrongEmail = false;
 // bool _wrongPassword = false;
@@ -24,9 +27,30 @@ class Measurement extends StatefulWidget {
 
 class _MeasurementState extends State<Measurement> {
   bool _showSpinner = false;
+  late int result;
+
+  void displayDialog(BuildContext context) {
+    Random random = new Random();
+    result = random.nextInt(100);
+
+    showDialog(
+        context: context,
+        builder: (BuildContext dialogContext) {
+          return MyPopup(result);
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 80,
+        title: Text('R A K S H A K', style: TextStyle(fontSize: 40)),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 93, 23, 105),
+        foregroundColor: Colors.white,
+        elevation: 1.0,
+      ),
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
       body: ModalProgressHUD(
@@ -40,37 +64,49 @@ class _MeasurementState extends State<Measurement> {
             ),
             Padding(
               padding: EdgeInsets.only(
-                  top: 60.0, bottom: 20.0, left: 20.0, right: 20.0),
+                  top: 60.0, bottom: 70.0, left: 20.0, right: 20.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(
-                    'RAKSHAK',
-                    style: TextStyle(fontSize: 50.0),
-                  ),
+                  // Text(
+                  //   'RAKSHAK',
+                  //   style: TextStyle(fontSize: 50.0),
+                  // ),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
                         'Name: ',
-                        style: TextStyle(fontSize: 30.0),
+                        style: TextStyle(fontSize: 25.0),
                       ),
+                      SizedBox(height: 30),
                       Text(
                         'Email: ',
-                        style: TextStyle(fontSize: 30.0),
+                        style: TextStyle(fontSize: 25.0),
                       ),
                     ],
                   ),
+                  // Column(
+                  //   crossAxisAlignment: CrossAxisAlignment.start,
+                  //   children: [
+                  //     Text(
+                  //       'Email: ',
+                  //       style: TextStyle(fontSize: 25.0),
+                  //     ),
+                  //   ],
+                  // ),
                   Column(
                     children: [
                       Material(
                         child: InkWell(
-                          onTap: () {},
+                          onTap: () {
+                            displayDialog(context);
+                          },
                           child: ClipRRect(
                             borderRadius: BorderRadius.circular(5.0),
                             child: Image.asset('assets/images/HeartRate.png',
-                                width: 300.0, height: 200.0),
+                                width: 305.0, height: 200.0),
                           ),
                         ),
                       )
@@ -80,11 +116,13 @@ class _MeasurementState extends State<Measurement> {
                     children: [
                       Material(
                         child: InkWell(
-                          onTap: () {},
+                          onTap: () {
+                            displayDialog(context);
+                          },
                           child: ClipRRect(
                             borderRadius: BorderRadius.circular(5.0),
                             child: Image.asset('assets/images/POTMeasure.png',
-                                width: 300.0, height: 200.0),
+                                width: 360.0, height: 155.0),
                           ),
                         ),
                       )
@@ -95,6 +133,40 @@ class _MeasurementState extends State<Measurement> {
             ),
           ],
         ),
+      ),
+      // bottomNavigationBar: BottomNavigationBar(
+      // toolbarHeight: 70,
+      bottomNavigationBar: SizedBox(
+        height: 70,
+        child: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed, // Fixed
+            backgroundColor:
+                Color.fromARGB(255, 93, 23, 105), // <-- This works for fixed
+            selectedItemColor: Color.fromARGB(255, 31, 1, 38),
+            unselectedItemColor: Color.fromARGB(255, 164, 100, 175),
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.library_books_rounded),
+                label: 'Manual',
+                backgroundColor: Color.fromARGB(255, 93, 23, 105),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.bookmark_add_rounded),
+                label: 'Reading',
+                backgroundColor: Color.fromARGB(255, 93, 23, 105),
+              ),
+              // ],
+              // currentIndex: _selectedIndex,
+              // selectedItemColor: Color.fromARGB(255, 255, 255, 255),
+              // onTap: _onItemTapped,
+            ]),
+        // ),
+        // items: const <BottomNavigationBarItem>[
+        // BottomNavigationBarItem(
+        //   icon: Icon(Icons.home),
+        //   label: 'Home',
+        //   backgroundColor: Color.fromARGB(255, 93, 23, 105),
+        // ),
       ),
     );
   }
